9df82f03f6e110ed74ceb90927235d78b02be7db
hodaa <hodaaahmed138@gmail.com>
2026-04-07 12:52:48 +0200

    add lesson and progress models

--- GraduationProject/LMSPhase01/LMSPhase01/Models/Lesson.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/Lesson.cs
@@ -6,5 +6,15 @@ namespace LMSPhase01.Models
 {
     public class Lesson
     {
+        public int Id { get; set; }
+
+        public int ModuleId { get; set; }
+        public string Title { get; set; }
+        public string VideoUrl { get; set; }
+        public int OrderIndex { get; set; }
+
+        // Navigation
+        public Module Module { get; set; }
+        public List<Progress> Progresses { get; set; } = new List<Progress>();
     }
 }
--- GraduationProject/LMSPhase01/LMSPhase01/Models/Progress.cs
+++ GraduationProject/LMSPhase01/LMSPhase01/Models/Progress.cs
@@ -6,5 +6,16 @@ namespace LMSPhase01.Models
 {
     public class Progress
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public int LessonId { get; set; }
+
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedAt { get; set; }
+
+        // Navigation
+        public ApplicationUser User { get; set; }
+        public Lesson Lesson { get; set; }
     }
 }
